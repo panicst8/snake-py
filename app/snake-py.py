87e651d87524c9d09a0e1d72c2011e0dc2fd00e9
Chris Milford <panicst8@yahoo.com>
2021-03-20 00:42:16 -0500

    inital pygame win working

--- app/snake-py.py
+++ app/snake-py.py
@@ -3,6 +3,7 @@
 import math
 import random
 import pygame
+from pygame.locals import *
 import tkinter as tk
 from tkinter import messagebox
 from typing import Any
@@ -45,12 +46,23 @@ class Snake(object):
         """ draw method """
 
 
-def drawGrid(w: Any, rows: Any, surface: Any) -> Any:
+def drawGrid(height: int, width: int, rows: int, win: Any) -> Any:
     """ drawGrid function """
+    size_between = width // rows
+    x = 0
+    y = 0
+    for _ in range(rows):
+        x = x + size_between
+        y = y + size_between
+        pygame.draw.line(win, (255, 255, 255), (x, 0), (x, height))
+        pygame.draw.line(win, (255, 255, 255), (0, y), (width, y))
 
 
-def redrawWindow(surface: Any) -> Any:
+def redrawWindow(win: Any, height: int, width: int, rows: int) -> Any:
     """ redrawWindow function """
+    win.fill((0, 0, 0))
+    drawGrid(height, width, rows, win)
+    pygame.display.update()
 
 
 def randomSnack(rows: Any, items: Any) -> Any:
@@ -66,13 +78,20 @@ def main() -> None:
     width = 500
     height = 500
     rows = 20
-    win = pygame.display.set_mode((width, height))
     s = Snake((255, 0, 0), (10, 10))
+
+    pygame.init()
+    win = pygame.display.set_mode((width, height))
+    pygame.display.set_caption("Snake -- The Game")
     run = True
     clock = pygame.time.Clock()
+    pygame.event.get()  # TODO: remove once event loop setup later
+
     while run:
         pygame.time.delay(50)
         clock.tick(10)
+
+        redrawWindow(win, height, width, rows)
 
 
 if __name__ == "__main__":
